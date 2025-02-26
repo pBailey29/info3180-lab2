3252d9ef85e869ee81a21db3ed19b2fdaea89fa8
pBailey29 <paulbaileyuni@gmail.com>
2025-02-26 13:21:14 -0500

    Added Profile Page

--- app/views.py
+++ app/views.py
@@ -1,6 +1,9 @@
 from app import app
 from flask import render_template, request, redirect, url_for, flash
+from datetime import datetime
 
+def format_date_joined(date):
+    return date.strftime("%B, %Y")  # "January, 2018"
 
 ###
 # Routing for your application.
@@ -15,8 +18,23 @@ def home():
 @app.route('/about/')
 def about():
     """Render the website's about page."""
-    return render_template('about.html', name="Mary Jane")
+    return render_template('about.html', name="Paul Bailey")
 
+@app.route('/profile')
+def profile():
+    """Render the website's profile page."""
+    user = {
+        "full_name": "Paul Bailey",
+        "username": "pbailey29",
+        "location": "Kingston, Jamaica",
+        "date_joined": format_date_joined(datetime(2018, 1, 10)),
+        "bio": "I am a smart and talented young man who loves website design and development. Contact me if you'd like to work together on a new project.",
+        "posts": 7,
+        "following": 100,
+        "followers": 250,
+        "profile_pic": "profile.jpg"
+    }
+    return render_template('profile.html', user=user)
 
 ###
 # The functions below should be applicable to all Flask apps.
